dccc100f59aeab25b0d6550e7c040844870c2b52
Lena <64004808+LenaPtak@users.noreply.github.com>
2021-02-22 20:13:33 +0100

    Add files via upload

---
+++ Projekt1/hello/World.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.Scanner;
+
+public class World {
+        public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String fellow = "World";
+        System.out.printf("Hello␣%s\n", fellow);
+
+        while(fellow!="exit") {
+            System.out.printf("Introduce␣yourself: ");
+            fellow = s.nextLine();
+            System.out.printf("Hello %s\n", fellow);
+        }
+        s.close();
+    }
+}
